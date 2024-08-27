9f797cc0efbf6aabfa9fe83bc1adb4300bbc95e7
chan <986311827@qq.com>
2024-08-27 23:56:41 +0800

    init

---
+++ app/src/main/java/com/example/xposeddemo/Hook.java
@@ -0,0 +1,9 @@
+package com.example.xposeddemo;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+public class Hook implements IXposedHookLoadPackage{
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+    }
+}
